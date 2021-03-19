a94a21d92707be546372c7031f1c742683af40a6
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-03-19 16:06:34 -0400

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -4,6 +4,6 @@ OS=`hostnamectl | grep "Operating System" | cut -f1 -d":" --complement | cut -c2
 KERNEL=`uname -r`
 UPTIME=`cat /proc/uptime`
 # Show output
-echo 'OS     $OS'
-echo 'Kernel $OS'
-echo 'Uptime $UPTIME'
+echo 'OS     '$OS''
+echo 'Kernel '$OS''
+echo 'Uptime '$UPTIME''
